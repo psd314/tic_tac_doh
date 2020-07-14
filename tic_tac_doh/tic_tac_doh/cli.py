9f4faca3dafba79452cb62f8476561434e25e608
Philippe Dixon <pdixon555@hotmail.com>
2020-07-13 21:55:59 -0400

    Add some command line arguments.

--- tic_tac_doh/tic_tac_doh/cli.py
+++ tic_tac_doh/tic_tac_doh/cli.py
@@ -1,19 +1,23 @@
 """Console script for tic_tac_doh."""
 import argparse
-import sys
-
+from .game_menu import menu
 
 def main():
     """Console script for tic_tac_doh."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
+
+    parser.add_argument('game', choices=['tic', 'connect'], help='Select game.')
+    parser.add_argument('action', choices=['play', 'train'], help='Play or train the AI.')
+    parser.add_argument('-p', '--policy', default='default data included with package',
+                        help='File path to policy for agent to play with.')
+    parser.add_argument('-e', '--episodes', default=10000, help='Number of episodes for AI to train.')
     args = parser.parse_args()
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "tic_tac_doh.cli.main")
+    print("Arguments: " + str(args))
+    if args.game == 'tic' and args.action == 'play':
+        menu()
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    main()  # pragma: no cover
---
+++ tic_tac_doh/tic_tac_doh/game_menu.py
@@ -0,0 +1,4 @@
+def menu():
+    print('Welcome to impossible Tic Tac Toe. Would you like to be X\'s (Player 1) or O\'s (Player 2)? x/o')
+    token = input()
+    print(token)
